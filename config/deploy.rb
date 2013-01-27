866916923cd96ac9c2b3c9121aa276ff36fd5226
Mr. Matt <matt@composition9.com>
2013-01-26 22:24:20 -0700

    get all ready for capistrano production deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,8 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 require 'capistrano/ext/multistage'
-set :stages, %w( murder )
-set :default_stage, 'murder'
+set :stages, %w( staging production )
+set :default_stage, 'staging'
 
 require 'rvm/capistrano'
 set :rvm_ruby_string, '1.9.3-p374'
@@ -14,29 +14,11 @@ set :rvm_type, :system
 
 require 'bundler/capistrano'
 
-set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, :git
 
 server 'murder', :app, :web, :db, primary: true
 
-#after 'deploy:update_code' do
-#  run "ln -nsf #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
-#end
-
-# if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
 
 before 'deploy:assets:precompile' do
   run "ln -nfs #{deploy_to}/shared/config/application.yml #{release_path}/config/application.yml"
